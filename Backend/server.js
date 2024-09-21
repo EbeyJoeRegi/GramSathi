f10a181c5943af5d2aad3ab189603c62712b1055
Ebey Joe Regi <107060884+EbeyJoeRegi@users.noreply.github.com>
2024-09-21 12:12:18 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'PASS@word123',
+  password: 'PASS@word123', //password here
   database: 'village_app'
 });
 
