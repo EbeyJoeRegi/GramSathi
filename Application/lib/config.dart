3d5dcf52729f61a47cecdf5a2ff8534d38f2fe07
Ebey Joe Regi <ebeyjoeregi13@gmail.com>
2024-10-18 22:39:48 +0530

    Revert "Backend update for otp testing"
    This reverts commit 56bc84c19aca23381e8cea322b0b2b51032b9ceb.

--- Application/lib/config.dart
+++ Application/lib/config.dart
@@ -1,5 +1,3 @@
 class AppConfig {
-  //static const String baseUrl = 'http://192.168.213.1:3000'; //CHINMAYEE
-  static const String baseUrl = 'http://192.168.5.1:3000'; //EbeyJoeRegi
-  //comment the unused baseUrl when not in use
+  static const String baseUrl = 'http://192.168.5.1:3000';
 }
