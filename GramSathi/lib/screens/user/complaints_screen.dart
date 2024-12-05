e3e135325887667bce7fca3f1c1d6c03f5d78730
Chinmayee1103 <chinmayee.bhat.n@gmail.com>
2024-12-05 22:26:38 +0530

    feedback hub-complaint and enquiry scree

--- GramSathi/lib/screens/user/complaints_screen.dart
+++ GramSathi/lib/screens/user/complaints_screen.dart
@@ -29,7 +29,7 @@ class _ComplaintsScreenState extends State<ComplaintsScreen> {
     try {
       final response = await http.get(
         Uri.parse(
-            '${AppConfig.baseUrl}/queries?username=${widget.username}&type=2'), // type=2 for complaints
+            '${AppConfig.baseUrl}/queries?username=${widget.username}&type=2'),
       );
 
       if (response.statusCode == 200) {
@@ -64,7 +64,7 @@ class _ComplaintsScreenState extends State<ComplaintsScreen> {
       'username': widget.username,
       'matter': matter,
       'time': DateTime.now().toIso8601String(),
-      'type': '2', // type=2 for complaints
+      'type': '2',
     };
 
     try {
@@ -99,116 +99,141 @@ class _ComplaintsScreenState extends State<ComplaintsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
+      backgroundColor: Color(0xFFE6F4E3),
+      body: Column(
         children: [
-          // Background Image
-          Positioned.fill(
-            child: Opacity(
-              opacity: 0.5, // Adjust opacity here
-              child: Image.asset(
-                'assets/images/user.png',
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          // Content on top of background image
-          Column(
-            children: [
-              // AppBar(
-              //   title: Text(
-              //     'Complaints',
-              //     style: TextStyle(
-              //         color: Colors
-              //             .white), // Set text color to white for better contrast
-              //   ),
-              //   backgroundColor: Colors
-              //       .transparent, // Make the AppBar background transparent
-              //   elevation: 0, // Remove the shadow below the AppBar
-              // ),
-              Expanded(
-                child: _isLoading
-                    ? Center(child: CircularProgressIndicator())
-                    : _errorMessage.isNotEmpty
-                        ? Center(child: Text(_errorMessage))
-                        : ListView.builder(
-                            itemCount: _complaints.length,
-                            itemBuilder: (context, index) {
-                              final complaint = _complaints[index];
-                              final dateTimeUtc =
-                                  DateTime.parse(complaint['time']);
-                              final dateTimeIst = convertUtcToIst(dateTimeUtc);
+          Expanded(
+            child: _isLoading
+                ? Center(child: CircularProgressIndicator())
+                : _errorMessage.isNotEmpty
+                    ? Center(child: Text(_errorMessage))
+                    : ListView.builder(
+                        itemCount: _complaints.length,
+                        itemBuilder: (context, index) {
+                          final complaint = _complaints[index];
+                          final dateTimeUtc = DateTime.parse(complaint['time']);
+                          final dateTimeIst = convertUtcToIst(dateTimeUtc);
 
-                              final formattedDate =
-                                  DateFormat('dd-MM-yy').format(dateTimeIst);
-                              final formattedTime =
-                                  DateFormat('hh:mm a').format(dateTimeIst);
+                          final formattedDate =
+                              DateFormat('dd-MM-yy').format(dateTimeIst);
+                          final formattedTime =
+                              DateFormat('hh:mm a').format(dateTimeIst);
 
-                              return Container(
-                                margin: EdgeInsets.all(8.0),
-                                padding: EdgeInsets.all(8.0),
-                                decoration: BoxDecoration(
-                                  color: Colors.white.withOpacity(
-                                      0.8), // White background with opacity
-                                  border: Border.all(
-                                    color: Colors.grey[300]!,
-                                    width: 1.0,
-                                  ),
-                                  borderRadius: BorderRadius.circular(8.0),
+                          return Container(
+                            margin: EdgeInsets.all(8.0),
+                            padding: EdgeInsets.all(8.0),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(8.0),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.2),
+                                  blurRadius: 6.0,
+                                  spreadRadius: 1.0,
+                                  offset: Offset(0, 3),
                                 ),
-                                child: ListTile(
-                                  title: Text(
-                                    complaint['matter'],
-                                    style: TextStyle(
-                                      fontSize: 18, // Increased font size
-                                    ),
-                                  ),
-                                  subtitle: Text(
+                              ],
+                            ),
+                            child: ListTile(
+                              leading: Icon(
+                                complaint['admin_response'] != null
+                                    ? Icons.check_circle
+                                    : Icons.pending,
+                                color: complaint['admin_response'] != null
+                                    ? Colors.green
+                                    : Colors.green,
+                              ),
+                              title: Text(
+                                complaint['matter'],
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              subtitle: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
                                     '$formattedDate • $formattedTime',
                                     style: TextStyle(
-                                      fontSize: 14,
+                                      fontStyle: FontStyle.italic,
                                       color: Colors.grey[700],
                                     ),
                                   ),
-                                  onTap: () {
-                                    _showComplaintDialog(complaint);
-                                  },
-                                ),
-                              );
-                            },
-                          ),
-              ),
-              Container(
-                margin: EdgeInsets.all(0),
-                padding: EdgeInsets.all(8.0),
-                decoration: BoxDecoration(
-                  color: Colors.white
-                      .withOpacity(1), // White background with opacity
-                  border: Border.all(
-                    color: Colors.grey[300]!,
-                    width: 1.0,
+                                  AnimatedOpacity(
+                                    opacity: complaint['admin_response'] != null
+                                        ? 1.0
+                                        : 0.5,
+                                    duration: Duration(seconds: 1),
+                                    child: Text(
+                                      complaint['admin_response'] ??
+                                          'Awaiting response',
+                                      style: TextStyle(
+                                        color:
+                                            complaint['admin_response'] != null
+                                                ? Colors.black
+                                                : Colors.red,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              onTap: () {
+                                _showComplaintDialog(complaint);
+                              },
+                            ),
+                          );
+                        },
+                      ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              padding: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: Color(0xFF015F3E).withOpacity(0.1),
+                borderRadius: BorderRadius.circular(12),
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0xFF015F3E).withOpacity(0.1),
+                    blurRadius: 8,
+                    offset: Offset(0, 2),
+                    spreadRadius: 1,
                   ),
-                  borderRadius: BorderRadius.circular(0),
-                ),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: TextField(
-                        controller: _complaintController,
-                        decoration: InputDecoration(
-                          labelText: 'Enter your complaint',
-                          border: OutlineInputBorder(),
+                ],
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _complaintController,
+                      decoration: InputDecoration(
+                        hintText: 'Enter your enquiry',
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide.none,
+                        ),
+                        contentPadding: EdgeInsets.symmetric(
+                          vertical: 12,
+                          horizontal: 20,
                         ),
                       ),
                     ),
-                    SizedBox(width: 8),
-                    IconButton(
-                      icon: Icon(Icons.send, color: Color(0xff015F3E)),
-                      onPressed: _createComplaint,
+                  ),
+                  SizedBox(width: 12),
+                  FloatingActionButton(
+                    onPressed: _createComplaint,
+                    backgroundColor: Color(0xff015F3E),
+                    child: Icon(
+                      Icons.send,
+                      color: Colors.white,
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ],
       ),
@@ -234,7 +259,7 @@ class _ComplaintsScreenState extends State<ComplaintsScreen> {
               Text(
                 complaint['matter'],
                 style: TextStyle(
-                  fontSize: 18, // Increased font size
+                  fontSize: 18,
                 ),
               ),
               SizedBox(height: 16),
--- GramSathi/lib/screens/user/suggestions_screen.dart
+++ GramSathi/lib/screens/user/suggestions_screen.dart
@@ -79,140 +79,171 @@ class _SuggestionsScreenState extends State<SuggestionsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   // title: Text(
-      //   //   'Suggestions',
-      //   //   style: TextStyle(color: Colors.teal), // AppBar title text is teal
-      //   // ),
-      //   backgroundColor: Colors.transparent, // Transparent AppBar background
-      //   elevation: 0, // No shadow
-      // ),
-      body: Stack(
-        children: [
-          // Background image
-          Positioned.fill(
-            child: Image.asset(
-              'assets/images/user.png', // Path to your background image
-              fit: BoxFit.cover,
-              color: const Color.fromARGB(255, 255, 255, 255)
-                  .withOpacity(0.5), // Apply opacity to background
-              colorBlendMode: BlendMode.lighten,
-            ),
-          ),
-          // Content
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: _suggestions.length,
-                    itemBuilder: (context, index) {
-                      final suggestion = _suggestions[index];
-                      return Container(
-                        margin: EdgeInsets.only(bottom: 16.0),
-                        padding: EdgeInsets.all(8.0),
-                        decoration: BoxDecoration(
-                          color: Colors.white.withOpacity(
-                              0.8), // White background with opacity
-                          border: Border.all(
-                            color: Colors.grey[300]!,
-                            width: 1.0,
-                          ),
-                          borderRadius: BorderRadius.circular(8.0),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: _suggestions.length,
+                itemBuilder: (context, index) {
+                  final suggestion = _suggestions[index];
+                  return Container(
+                    margin: EdgeInsets.only(bottom: 16.0),
+                    padding: EdgeInsets.all(12.0),
+                    decoration: BoxDecoration(
+                      color: Color(0xFFE6F4E3), // Light green background
+                      borderRadius: BorderRadius.circular(12.0),
+                      border: Border.all(
+                        color: Color(0xff005F3D).withOpacity(
+                            0.5), // Lighter and more spread out border
+                        width: 2.0, // Increased width for a more spread look
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color(0xff005F3D).withOpacity(
+                              0.2), // Subtle shadow of the same border color
+                          spreadRadius: 1, // Increased spread
+                          blurRadius: 3, // Soft blur effect for the shadow
+                          offset: Offset(0, 4),
                         ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+                      ],
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
                             Text(
                               suggestion['title'] ?? 'No Title',
                               style: TextStyle(
-                                fontSize: 18,
+                                fontSize: 20,
                                 fontWeight: FontWeight.bold,
-                                color: Colors.grey[800], // Title color
+                                color: Color(0xff005F3D),
                               ),
                             ),
-                            SizedBox(height: 8.0),
-                            Text(
-                              'By ${suggestion['username'] ?? 'Unknown'} on ${DateFormat.yMMMd().format(DateTime.parse(suggestion['created_at'] ?? DateTime.now().toIso8601String()))}',
-                              style: TextStyle(
-                                color: Colors.grey[600], // Date line color
-                              ),
-                            ),
-                            SizedBox(height: 8.0),
-                            Text(
-                              suggestion['content'] ?? 'No Content',
-                              style: TextStyle(
-                                color: Colors.grey[800], // Content color
-                              ),
-                            ),
-                            SizedBox(height: 8.0),
-                            Text(
-                              suggestion['response'] == null ||
-                                      suggestion['response'].isEmpty
-                                  ? 'Waiting for Admin response'
-                                  : 'Admin : ${suggestion['response']}',
-                              style: TextStyle(
-                                color: suggestion['response'] == null ||
-                                        suggestion['response'].isEmpty
-                                    ? Colors.red
-                                    : Colors.black,
-                              ),
-                            ),
+                            Icon(Icons.more_vert, color: Colors.green[600]),
                           ],
                         ),
-                      );
-                    },
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.all(16.0),
-                  decoration: BoxDecoration(
-                    color: Colors.white
-                        .withOpacity(0.8), // White background with opacity
-                    border: Border.all(
-                      color: Colors.grey[300]!,
-                      width: 1.0,
+                        SizedBox(height: 8.0),
+                        Text(
+                          'By ${suggestion['username'] ?? 'Unknown'} on ${DateFormat.yMMMd().format(DateTime.parse(suggestion['created_at'] ?? DateTime.now().toIso8601String()))}',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey[600],
+                          ),
+                        ),
+                        SizedBox(height: 8.0),
+                        Text(
+                          suggestion['content'] ?? 'No Content',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey[800],
+                          ),
+                        ),
+                        SizedBox(height: 8.0),
+                        Text(
+                          suggestion['response'] == null ||
+                                  suggestion['response'].isEmpty
+                              ? 'Waiting for Admin response'
+                              : 'Admin : ${suggestion['response']}',
+                          style: TextStyle(
+                            color: suggestion['response'] == null ||
+                                    suggestion['response'].isEmpty
+                                ? Colors.red
+                                : Colors.black,
+                            fontStyle: FontStyle.italic,
+                          ),
+                        ),
+                      ],
                     ),
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  child: Column(
-                    children: [
-                      TextField(
-                        controller: _titleController,
-                        decoration: InputDecoration(
-                          labelText: 'Title',
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
-                      SizedBox(height: 8.0),
-                      TextField(
-                        controller: _contentController,
-                        decoration: InputDecoration(
-                          labelText: 'Content',
-                          border: OutlineInputBorder(),
-                        ),
-                        maxLines: 4,
-                      ),
-                      SizedBox(height: 16.0),
-                      ElevatedButton(
-                        onPressed: _postSuggestion,
-                        child: Text('Post Suggestion'),
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor:
-                              Color(0xff015F3E), // Button background color
-                          foregroundColor: Colors.white, // Button text color
-                          padding: EdgeInsets.symmetric(
-                              vertical: 12.0, horizontal: 16.0),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+                  );
+                },
+              ),
             ),
-          ),
-        ],
+            Container(
+              padding: EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                color: Color(
+                    0xffF7F7F7), // Soft light gray background for more separation
+                borderRadius: BorderRadius.circular(12.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(
+                        0.8), // Slightly darker shadow for better separation
+                    spreadRadius: 5,
+                    blurRadius: 10,
+                    offset: Offset(0, 6), // More pronounced shadow effect
+                  ),
+                ],
+              ),
+              margin: EdgeInsets.symmetric(
+                  vertical: 16.0), // Adds space around the form
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Title TextField
+                  TextField(
+                    controller: _titleController,
+                    decoration: InputDecoration(
+                      labelText: 'Title',
+                      labelStyle: TextStyle(color: Color(0xff005F3D)),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                        borderSide: BorderSide(color: Colors.green[700]!),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 16.0), // Increased space to prevent cramping
+
+                  // Content TextField
+                  TextField(
+                    controller: _contentController,
+                    decoration: InputDecoration(
+                      labelText: 'Content',
+                      labelStyle: TextStyle(color: Color(0xff005F3D)),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                        borderSide: BorderSide(color: Colors.green[700]!),
+                      ),
+                    ),
+                    maxLines: 4,
+                  ),
+                  SizedBox(
+                      height:
+                          20.0), // Added extra space to separate the button further
+
+                  // Submit Button
+                  Center(
+                    child: ElevatedButton(
+                      onPressed: _postSuggestion,
+                      child: Text('Post Suggestion'),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.lerp(Color(0xff015F3E),
+                            Colors.white, 0.1), // 30% blend with white
+
+// Button background color
+                        foregroundColor: Colors.white, // Button text color
+                        padding: EdgeInsets.symmetric(
+                            vertical: 12.0, horizontal: 16.0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
