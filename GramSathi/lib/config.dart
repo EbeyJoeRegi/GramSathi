9bb5c7b867cdd8a6a0192be926104976d17e14d3
Ebey Joe Regi <ebeyjoeregi13@gmail.com>
2025-01-01 19:02:49 +0530

    Final Version

--- GramSathi/lib/config.dart
+++ GramSathi/lib/config.dart
@@ -1,6 +1,3 @@
 class AppConfig {
-  //static const String baseUrl = 'http://192.168.213.1:3000'; //CHINMAYEE
-  static const String baseUrl = 'http://192.168.5.1:3000'; //EbeyJoeRegi
-  //static const String baseUrl ='https://rural-nexus-api.onrender.com'; //Public IP
-  //comment the unused baseUrl when not in use
+  static const String baseUrl = 'http://localhost:3000';
 }
